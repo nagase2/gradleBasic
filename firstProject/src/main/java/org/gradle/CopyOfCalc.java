d62da2c714dca7c779025c0c8cca2af5ccf58a61
na <yoshimasa_nagase@tsyscom.co.jp>
2015-05-14 18:09:45 +0900

    Sonar

---
+++ firstProject/src/main/java/org/gradle/CopyOfCalc.java
@@ -0,0 +1,11 @@
+package org.gradle;
+
+public class CopyOfCalc {
+	public int add(int a, int b){
+		
+		return a+b;
+	}
+	public int multiply(int a,int b){
+		return a*b;
+	}
+}
---
+++ firstProject/src/main/java/org/gradle/StringManager.java
@@ -0,0 +1,11 @@
+package org.gradle;
+
+public class StringManager {
+	public String add(int a, int b){
+		
+		return "add";
+	}
+	public String multiply(int a,int b){
+		return "multi";
+	}
+}
